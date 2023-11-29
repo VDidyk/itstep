cd138e0daf55b5749348ee8a815d5e92a330e233
VDidyk <Tailer12012@gmail.com>
2023-11-29 14:26:55 +0100

    # Користувач вводить з клавіатури набір рядків. Збережіть отримані рядки до двозв’язного списку. Після чого # покажіть меню, в якому запропонуєте користувачеві # набір пунктів: # 1. Додати елемент до списку. # 2. Видалити елемент зі списку. # Практичне завдання # 1 # 3. Показати вміст списку. # 4. Перевірити, чи є значення у списку. # 5. Замінити значення у списку. # Дія виконується залежно від вибору користувача, # після чого меню з’являється знову.

--- Practical/Structures-P-P1/2.py
+++ Practical/Structures-P-P1/2.py
@@ -1,19 +1,21 @@
-# Користувач вводить з клавіатури набір чисел. Збережіть отримані числа до однозв’язного списку. Після
-# чого покажіть меню, в якому запропонуєте користувачеві
+# Користувач вводить з клавіатури набір рядків. Збережіть отримані рядки до двозв’язного списку. Після чого
+# покажіть меню, в якому запропонуєте користувачеві
 # набір пунктів:
 # 1. Додати елемент до списку.
 # 2. Видалити елемент зі списку.
+# Практичне завдання
+# 1
 # 3. Показати вміст списку.
 # 4. Перевірити, чи є значення у списку.
 # 5. Замінити значення у списку.
-# Дія виконується залежно від вибору користувача, після чого меню з’являється знову
+# Дія виконується залежно від вибору користувача,
+# після чого меню з’являється знову.
 
-
-# Синтаксис Однозв'язковий список
 class Node:
     def __init__(self, data):
         self.data = data
         self.__next = None
+        self.__prev = None
 
     @property
     def next(self):
@@ -23,8 +25,16 @@ class Node:
     def next(self, node: super):
         self.__next = node
 
+    @property
+    def prev(self):
+        return self.__prev
+
+    @prev.setter
+    def prev(self, node: super):
+        self.__prev = node
+
     def __str__(self):
-        return f"[{self.data}] -> {self.__next}"
+        return f"{self.prev.data if self.prev else None} -> [{self.data}] -> {self.next.data if self.next else None}"
 
 
 class LinkedList:
@@ -39,6 +49,7 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+        new_node.prev = last_node
 
     def each(self, callback):
         head = self.head
@@ -52,11 +63,18 @@ class LinkedList:
 
             if node is None and prev_node is not None:
                 prev_node.next = head.next
+                if head.next:
+                    head.next.prev = prev_node
             elif node is None:
                 prev_node = None
+                if head.next:
+                    head.next.prev = prev_node
+
                 self.head = head.next
             else:
                 prev_node = head
+                if head.next:
+                    head.next.prev = prev_node
 
             head = head.next
 
@@ -93,19 +111,24 @@ class LinkedList:
 
         self.each(replace_callback)
 
-    def __str__(self):
-        return f"{self.head}"
+    def print(self):
+        def print_callback(node):
+            print(node)
+
+            return True
+
+        self.each(print_callback)
 
 
 my_lst = LinkedList()
 nums = input("Enter numbers separated by space: ").split()
 for num in nums:
     my_lst.append(int(num))
-print(my_lst)
+print(my_lst.print())
 
 my_lst.remove(2)
-print(my_lst)
+print(my_lst.print())
 
 print(my_lst.check(3))
 my_lst.replace(5, 15)
-print(my_lst)
+print(my_lst.print())
